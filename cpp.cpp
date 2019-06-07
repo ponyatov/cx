6e1fd22d84c99914db2beed23cf8a1722866b8f8
Dmitry Ponyatov <dponyatov@gmail.com>
2019-06-07 10:40:14 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cpp.cpp
+++ cpp.cpp
@@ -5,9 +5,28 @@ void yyerror(string msg) { cout<<YYERR; cerr<<YYERR; exit(-1); }
 
 int main() { return yyparse(); }
 
-Frame::Frame(string T, string V) { tag = T; val = V; }
+Frame::Frame(string T, string V) { tag = T; val = V; ref=0; }
 Frame::Frame(string V):Frame("frame",V) {}
 
+string Frame::dump(int depth, string prefix) {
+	string tree = pad(depth) + head(prefix);
+	for (auto it=nest.begin(),e=nest.end(); it!=e; it++)
+		tree += (*it)->dump(depth+1);
+	return tree;
+}
 
-string Frame::dump(int depth, string prefix) { return head(prefix); }
 string Frame::head(string prefix) { return prefix+"<"+tag+":"+val+">"; }
+
+string Frame::pad(int depth) {
+	string tabs = "\n";
+	for (int i = 0; i < depth; i++) tabs += "\t";
+	return tabs;
+}
+
+void Frame::push(Frame *o) { nest.push_back(o); }
+
+Sym::Sym(string V):Frame("sym",V) {}
+
+Str::Str(string V):Frame("str",V) {}
+
+Op::Op(string V):Frame("op",V) {}
--- hpp.hpp
+++ hpp.hpp
@@ -2,49 +2,81 @@
 #define _H_CX
 
 #include <iostream>
-#include <string>
+#include <ostream>
+#include <map>
+#include <vector>
 
 using namespace std;
 
 /// @defgroup frame extended Marvin Minsky frame model
-/// @{
 
-						/// base frame class
+/// @ingroup frame
+								/// base frame class (generic object)
 struct Frame {
-						/// type/class tag
+								/// type/class tag
 	string tag;
-						/// value
+								/// value
 	string val;
+								/// slot{}s = attributes
+	map<string,Frame*> slot;
+								/// nest[]ed elements
+	vector<Frame*> nest;
+								/// ref.counter for GC
+	long ref;
 
 								/// frame constructor with given type
 	Frame(string T, string V);
 								/// frome constructor from value only
 	Frame(string V);
 
+	/// dump in full tree form
 	string dump(int depth=0, string prefix="");
+
+	/// dump header only
 	string head(string prefix="");
 
+	/// left pad with tabs
+	string pad(int depth);
+									/// push into @ref nest as a stack
+	void push(Frame *o);
+
 };
+
+/// @defgroup prim Primitives
+/// @ingroup frame
+/// @{
+									/// symbol
+struct Sym: Frame { Sym(string); };
+									/// string
+struct Str: Frame { Str(string); };
+
+/// @}
+
+/// @defgroup meta Metaprogramming
+/// @ingroup frame
+/// @{
+									/// operator
+struct Op: Frame { Op(string); };
 
 /// @}
 
 /// @defgroup parser parser interface
-/// @{
+
 									/// @name flex
+
 									/// lexer call gives single token
 extern int yylex();
 									/// current line number
 extern int yylineno;
-									/// current lexeme sleected by lexer
+									/// current lexeme selected by lexer
 extern char *yytext;
 									/// @name bison
+
 									/// parser start /bison/
 extern int yyparse();
 									/// parser error callback
 extern void yyerror(string msg);
 #include "ypp.tab.hpp"
-
-/// @}
 
 #endif // _H_CX
 
